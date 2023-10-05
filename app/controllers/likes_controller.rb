7b07aeaf1aeb7b696423234776eba97d81020f27
Alvaro Sanchez <hello@alvarosanchez.dev>
2023-10-05 17:05:44 -0600

    The like button and the remaining likes count update dynamically via turbo_stream

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,10 +1,18 @@
 class LikesController < ApplicationController
+  include ActionView::RecordIdentifier
   def update
     if likes.exists?
       likes.destroy_all
     else
       likes.first_or_create(user: current_user)
     end
+
+    turbo_streams = [
+      turbo_stream.replace("likes-count", partial: "remaining_likes", locals: { remaining_likes_quantity: User::LIKE_LIMIT - current_user.likes.count } ),
+      turbo_stream.replace(dom_id(post), partial: "posts/post", locals: { post: post })
+    ]
+    
+    render turbo_stream: turbo_streams
   end
 
   private
