c82b901d6f5155247749acfff540069780c6b99d
Alvaro Sanchez <hello@alvarosanchez.dev>
2023-10-05 17:45:02 -0600

    Likes controller prevents liking if the user has reached the limit

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -3,12 +3,14 @@ class LikesController < ApplicationController
   def update
     if likes.exists?
       likes.destroy_all
-    else
+    elsif current_user.likes.count < User::LIKE_LIMIT
       likes.first_or_create(user: current_user)
+    else
+      errors = "You've reached the like limit"
     end
 
     turbo_streams = [
-      turbo_stream.replace("likes-count", partial: "remaining_likes", locals: { remaining_likes_quantity: User::LIKE_LIMIT - current_user.likes.count } ),
+      turbo_stream.replace("top-card", partial: "posts/top_card", locals: { name: current_user.name, remaining_likes_quantity: User::LIKE_LIMIT - current_user.likes.count, errors: errors } ),
       turbo_stream.replace(dom_id(post), partial: "posts/post", locals: { post: post })
     ]
     
